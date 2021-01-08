b30097d558322de204a46b3b325aa6aeb49ebbd7
Dishant <dishantdua8956@gmail.com>
2021-01-08 17:14:47 +0530

    Add Expandable List on Main Screen

--- app/src/main/java/com/example/learnrevisetwo/MainActivity.java
+++ app/src/main/java/com/example/learnrevisetwo/MainActivity.java
@@ -9,10 +9,17 @@ import android.os.Bundle;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ExpandableListAdapter;
+import android.widget.ExpandableListView;
 import android.widget.TextView;
 import android.widget.Toast;
 
 import com.google.android.material.navigation.NavigationView;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -20,12 +27,57 @@ public class MainActivity extends AppCompatActivity {
     private ActionBarDrawerToggle actionBarDrawerToggle;
     private TextView txt;
     private Button btn;
+
+    private ExpandableListView expandableListView;
+    private List<String> langs;
+    private Map<String, List<String>> topics;
+    private ExpandableListAdapter listAdapter;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        drawerLayout = findViewById(R.id.drawerLayout);
-        txt = findViewById(R.id.txt);
+        createView();
+        setUpNavigationView();
+        fillExListData();
+
+        listAdapter = new MyExListAdapter(this, langs, topics);
+        expandableListView.setAdapter(listAdapter);
+        expandableListView.setGroupIndicator(null);
+        expandableListView.setChildDivider(null);
+        expandableListView.setDivider(null);
+
+        expandableListView.setOnChildClickListener(new ExpandableListView.OnChildClickListener() {
+            @Override
+            public boolean onChildClick(ExpandableListView parent, View v, int groupPosition, int childPosition, long id) {
+                Toast.makeText(MainActivity.this, topics.get(langs.get(groupPosition)).get(childPosition), Toast.LENGTH_SHORT).show();
+                return false;
+            }
+        });
+    }
+
+    private void fillExListData() {
+        langs = new ArrayList<>();
+        topics = new HashMap<>();
+
+        langs.add("C");
+        langs.add("java");
+
+        List<String> java = new ArrayList<>();
+        java.add("Super");
+        java.add("Encapsulation");
+        java.add("Inheritance");
+
+        List<String> c = new ArrayList<>();
+        c.add("Method");
+        c.add("Pointers");
+        c.add("Arrrays");
+
+        topics.put(langs.get(0),c);
+        topics.put(langs.get(1),java);
+
+    }
+
+    private void setUpNavigationView() {
         actionBarDrawerToggle = new ActionBarDrawerToggle(this, drawerLayout,R.string.Open, R.string.Close);
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
         drawerLayout.addDrawerListener(actionBarDrawerToggle);
@@ -60,6 +112,12 @@ public class MainActivity extends AppCompatActivity {
                 return true;
             }
         });
+    }
+
+    private void createView() {
+        drawerLayout = findViewById(R.id.drawerLayout);
+        expandableListView = findViewById(R.id.expandableListView);
+        txt = findViewById(R.id.txt);
     }
 
     @Override
---
+++ app/src/main/java/com/example/learnrevisetwo/MyExListAdapter.java
@@ -0,0 +1,87 @@
+package com.example.learnrevisetwo;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseExpandableListAdapter;
+import android.widget.ExpandableListAdapter;
+import android.widget.TextView;
+
+import java.util.List;
+import java.util.Map;
+
+public class MyExListAdapter extends BaseExpandableListAdapter {
+
+
+    private Context context;
+    private List<String> langs;
+    private Map<String, List<String>> topics;
+    public MyExListAdapter(Context context, List<String> langs, Map<String, List<String>> topics) {
+        this.context = context;
+        this.langs = langs;
+        this.topics = topics;
+    }
+
+    @Override
+    public int getGroupCount() {
+        return langs.size();
+    }
+
+    @Override
+    public int getChildrenCount(int groupPosition) {
+        return topics.get(langs.get(groupPosition)).size();
+    }
+
+    @Override
+    public Object getGroup(int groupPosition) {
+        return langs.get(groupPosition);
+    }
+
+    @Override
+    public Object getChild(int groupPosition, int childPosition) {
+        return topics.get(langs.get(groupPosition)).get(childPosition);
+    }
+
+    @Override
+    public long getGroupId(int groupPosition) {
+        return groupPosition;
+    }
+
+    @Override
+    public long getChildId(int groupPosition, int childPosition) {
+        return childPosition;
+    }
+
+    @Override
+    public boolean hasStableIds() {
+        return false;
+    }
+
+    @Override
+    public View getGroupView(int groupPosition, boolean isExpanded, View convertView, ViewGroup parent) {
+        if (convertView == null) {
+            LayoutInflater layoutInflater = (LayoutInflater) context.getSystemService(context.LAYOUT_INFLATER_SERVICE);
+            convertView = layoutInflater.inflate(R.layout.list_parent, parent, false);
+        }
+        TextView txt = convertView.findViewById(R.id.txt);
+        txt.setText((String)getGroup(groupPosition));
+        return convertView;
+    }
+
+    @Override
+    public View getChildView(int groupPosition, int childPosition, boolean isLastChild, View convertView, ViewGroup parent) {
+        if(convertView == null) {
+            LayoutInflater layoutInflater = (LayoutInflater) context.getSystemService(context.LAYOUT_INFLATER_SERVICE);
+            convertView = layoutInflater.inflate(R.layout.list_child, parent, false);
+        }
+        TextView txt = convertView.findViewById(R.id.txt);
+        txt.setText((String)getChild(groupPosition, childPosition));
+        return convertView;
+    }
+
+    @Override
+    public boolean isChildSelectable(int groupPosition, int childPosition) {
+        return true;
+    }
+}
